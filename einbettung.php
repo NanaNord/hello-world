c5c6630c62a632e214132cc06deba8964b6e5f93
Nana <stud.nana@women-at-work.org>
2016-08-27 19:39:38 +0200

    Erste Version

---
+++ einbettung.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>Titelzeile der Datei</title>
+</head>
+<body>
+Die 1. Zeile in HTML<br>
+<?php echo "Die 2. Zeile in PHP<br>"; ?>
+Die 3. Zeile in HTML<br>
+<?php
+   echo "Die 4. Zeile in PHP<br>";
+   echo "Die 5. Zeile in PHP";
+?>
+</body></html>
